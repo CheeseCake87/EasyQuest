6e96a914fe9bf5514a10ad27cdd192cb4cae2956
David Carmichael <carmichaelits@gmail.com>
2023-03-30 11:16:22 +0100

    Changed structure, and adding in fundamentals.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_bigapp import BigApp
+from flask_sqlalchemy import SQLAlchemy
 
-# this import needs to be relative (.views.ind...) as views is not a package
-from .views.index import index_views
+bigapp = BigApp()
+db = SQLAlchemy()
 
 
 def create_app():
-    app = Flask(__name__)
-
-    index_views(app)
+    app = Flask(
+        __name__,
+        static_folder="global/static",
+        static_url_path="/global/static"
+    )
+    bigapp.init_app(app)
+    db.init_app(app)
 
     return app
