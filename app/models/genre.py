958634e79baa31f13fad09644f20d91b6cc7661d
David Carmichael <carmichaelits@gmail.com>
2023-04-03 09:25:58 +0100

    classmethod added to work with the new recommended way of working with sqlalchemy

--- app/models/genre.py
+++ app/models/genre.py
@@ -7,7 +7,26 @@ class Genre(db.Model):
 
     # Data
     genre = schema.Column(types.String(256), nullable=False)
-    description = schema.Column(types.String(512), nullable=False)
+    description = schema.Column(types.String(512), nullable=True)
 
     # Tracking
     created = schema.Column(types.DateTime, default=dater())
+
+    @classmethod
+    def create(cls, genre: str = None, description: str = None, batch: list[dict] = None):
+        if batch is not None:
+            result = db.session.scalars(
+                insert(cls).returning(cls),
+                batch
+            )
+            db.session.commit()
+            return result.all()
+
+        result = db.session.scalar(
+            insert(cls).returning(cls).values(
+                genre=genre,
+                description=description
+            )
+        )
+        db.session.commit()
+        return result
