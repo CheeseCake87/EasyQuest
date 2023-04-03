c061ecb08e4a4ffb96f1d49e785d35a03b10ed2e
David Carmichael <carmichaelits@gmail.com>
2023-04-03 09:23:49 +0100

    adjusted static folder definitions, added database creation with practice code on batch processing default data.

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,34 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(
         __name__,
-        static_folder="global/static",
-        static_url_path="/global/static"
+        static_folder="static",
+        template_folder="templates",
+        static_url_path="/static"
     )
     bigapp.init_app(app)
     db.init_app(app)
 
+    bigapp.import_builtins()
     bigapp.import_models(from_folder="models")
     bigapp.import_blueprints("blueprints")
 
     with app.app_context():
+        db.drop_all()
         db.create_all()
+        # result = bigapp.model("Genre").create(
+        #     batch=[
+        #         {"genre": "Action"},
+        #         {"genre": "Adventure"},
+        #         {"genre": "Comedy"},
+        #         {"genre": "Crime"},
+        #         {"genre": "Drama"},
+        #         {"genre": "Fantasy"},
+        #         {"genre": "Historical"},
+        #     ]
+        # )
+        result = bigapp.model("Genre").create(
+            genre="Horror",
+        )
+        print(result)
 
     return app
