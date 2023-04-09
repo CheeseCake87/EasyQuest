120b4ecdafc75a355575fd2eb625a6c91132389f
David Carmichael <carmichaelits@gmail.com>
2023-04-09 23:42:38 +0100

    Removed security class as this is the old method of decorating.

--- app/extensions/__init__.py
+++ app/extensions/__init__.py
@@ -1,13 +1,12 @@
 import configparser
 
 from flask import Flask
-from flask_bigapp import BigApp, Security, Auth
+from flask_bigapp import BigApp, Auth
 from flask_sqlalchemy import SQLAlchemy
 
 bigapp = BigApp()
 db = SQLAlchemy()
 sysconf = configparser.ConfigParser()
-security = Security()
 auth = Auth()
 
-__all__ = ["Flask", "bigapp", "db", "sysconf", "security", "auth"]
+__all__ = ["Flask", "bigapp", "db", "sysconf", "auth"]
